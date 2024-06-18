ec9d4f9d62daf31a3fa738aa0ab6a0024b034029
Alura Geek <gabriel.santana479@gmail.com>
2024-06-18 16:01:21 -0300

    sidebar finalizada

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
+import { Post } from "./Post";
 import { Header } from "./components/header";
+import styles from "./App.module.css";
 import "./global.css";
+import { Sidebar } from "./components/Sidebar";
 
 export function App() {
   return (
-    <>
+    <div>
       <Header />
-      <h1>Olá mundo</h1>
-    </>
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Roberto Gabriel"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio deleniti error perspiciatis illum, tempora assumenda sit sint dolore illo necessitatibus in ea laudantium nulla reprehenderit. Nam eum sint facilis delectus."
+          />
+          <Post author="Felipe" content="Um novo post muito legal" />
+        </main>
+      </div>
+    </div>
   );
 }
